fbe22b44f10305ad7e2d747728b3c3139dae73c7
harshit jain <harshit.33569@tmu.ac.in>
2024-11-08 13:18:28 +0530

    feat: added logrocket sdk

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import {Platform} from 'react-native';
 import * as Sentry from '@sentry/react-native';
 import {LogLevel, OneSignal} from 'react-native-onesignal';
 import Geolocation from '@react-native-community/geolocation';
+import LogRocket from '@logrocket/react-native';
 
 Sentry.init({
   dsn: 'https://66e762c5edd4f71ab2d9fae889060559@o4508255429984256.ingest.us.sentry.io/4508255686492160',
@@ -20,6 +21,8 @@ if (Platform.OS === 'ios' || Platform.OS === 'android') {
 
 const App = () => {
   useEffect(() => {
+    LogRocket.init('iwbft5/estore');
+    console.log('identify log rocket user', LogRocket.identify('harshit'));
     OneSignal.Debug.setLogLevel(LogLevel.Verbose);
     OneSignal.initialize('ea616c69-8c12-464f-bfaa-9ddcc0215d00');
     OneSignal.Notifications.requestPermission(true);
